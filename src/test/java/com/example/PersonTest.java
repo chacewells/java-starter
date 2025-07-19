4a42cc80ea39caccbdf956d1774c34a3b01330a4
Aaron Wells <titansrevolt@protonmail.com>
2025-07-19 10:57:26 -0700

    Add lombok; refactor Person

--- src/test/java/com/example/PersonTest.java
+++ src/test/java/com/example/PersonTest.java
@@ -1,16 +1,26 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PersonTest {
 
   @Test
   void testPerson() {
-    assertTrue(true, "this is totally fine");
+    var p = new Person();
+    p.setName("Aaron Bar Baz");
+    p.setCity("Broqualmie");
+    p.setAge(40);
+
+    assertEquals("Aaron Bar Baz", p.getName());
+    assertEquals("Broqualmie", p.getCity());
+    assertEquals(40, p.getAge());
+
+    assertTrue(p.toString().contains("Aaron Bar Baz"));
+    assertTrue(p.toString().contains("40"));
+    assertTrue(p.toString().contains("Broqualmie"));
   }
 
 }
